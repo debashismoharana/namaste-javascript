c08e92250d61378e1b1885907832b87efd4ba952
Debashis Moharana <moharana.debashis@gmail.com>
2025-03-30 10:58:38 +0530

    minor modification to logs

--- scripts/promise.js
+++ scripts/promise.js
@@ -1,23 +1,23 @@
 console.log("Hello from promise.js");
 
 const p1 = new Promise((resolve, reject) => {
-  resolve("Success p1");
+  resolve("P1: Success");
   setTimeout(() => {
     reject("Error p1");
   }, 1000);
 });
 
 const p2 = new Promise((resolve, reject) => {
-  resolve("Success p2");
+  resolve("P2: Success");
 });
 
 const p3 = new Promise((resolve, reject) => {
-  resolve("Success p3");
+  resolve("P3: Success");
 });
 
 Promise.all([p1, p2, p3])
   .then((res) => {
-    console.log(res);
+    console.log("All", res);
   })
   .catch((err) => {
     console.error(err);
@@ -36,5 +36,5 @@ Promise.allSettled([p1, p2, p3]).then((res) => {
 });
 
 Promise.any([p1, p2, p3]).then((res) => {
-  console.log("Any(1st success): ", res);
+  console.log("Any (1st success): ", res);
 });
